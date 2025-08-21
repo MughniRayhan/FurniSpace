6783891cdf8d7c26075ecb0d7e5c2c71cba95956
MughniRayhan <tishtisha13@gmail.com>
2025-08-21 16:12:12 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Welcome to Furni Space</h1>
-      <p className="mt-4">Your one-stop shop for all furniture needs.</p>
+      <h1 className="text-4xl font-bold">Hello world</h1>
     </div>
   )
 }
